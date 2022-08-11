d8ffa1a14b5f22ae792c48041096f47d1012adfc
sunkuyng <buxzz88@naver.com>
2022-08-11 09:19:51 +0900

    20220810:

--- ex02/src/main/java/org/zerock/service/BoardService.java
+++ ex02/src/main/java/org/zerock/service/BoardService.java
@@ -3,6 +3,7 @@ package org.zerock.service;
 import java.util.List;
 
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardService {
 	
@@ -20,4 +21,6 @@ public interface BoardService {
 	
 	//전체 게시글 select
 	public List<BoardVO> getList();
+	
+	public List<BoardVO> getList(Criteria cri);
 }
--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -90,6 +90,17 @@ public class BoardControllerTests {
 		log.info(resultpage);
 		
 	}
+	
+	@Test
+	public void testListPaging() throws Exception{
+		
+		log.info(mockMvc.perform(
+				MockMvcRequestBuilders.get("/board/list")
+				.param("pageNum", "2")
+				.param("amount", "50"))
+				.andReturn().getModelAndView().getModelMap()
+				);
+	}
 }
 	
 	
